bd326438decb7c1d4b1f39d53c01725abbc6558d
unknown <neduvinci@gmail.com>
2019-09-22 02:55:56 -0700

    successfully able to change pages by adding the api to state. going to perform cleanup now

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,37 +2,68 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import Characters from './components/Characters';
+import cutesy from 'styled-components';
 
+const CenterDiv = cutesy.div`
+ max-width: 1000px;
+ margin: 0 auto;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [peopleState, setPeople] = useState([]);
+  const [firstResultState, setFirst] = useState([]);
+  const [nextResultState, setNext] = useState([]);
+  const [currentApiState, setCurrentApi] = useState('https://swapi.co/api/people');
+  const [nextApiState, setNextApi] = useState('');
+  // console.log(apiState);
+  // console.log(currentApiState);
+  // console.log(nextApiState);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // const starwarsPeopleApi = "https://swapi.co/api/people";
+  const nextPage = evt => {
+    evt.preventDefault();
+    const newPage = [...nextResultState];
+    setCurrentApi(nextApiState);
+  }
+
+
   useEffect(() => {
-    axios.get("https://swapi.co/api/people")
+    axios.get(currentApiState)
       .then(swapiYes => {
         // debugger
-        const { results } = swapiYes['data'];
+        const { results, next } = swapiYes['data'];
         // debugger
-        // console.log(results);
-        
-        setPeople(results);
-        // debugger
-        // setName(swapiYes['data'][])
-    })
-  }, [])
+        // console.log(next);
+        setFirst(results);
+        setNextApi(next);
+        axios.get(next)
+          .then(nextYes => {
+            // debugger
+            const { results } = nextYes['data'];
+            setNext(results);
+          })
+
+
+
+      })
+
+
+  }, [currentApiState])
 
   return (
-    <div className="App">
+    <CenterDiv className="App">
       <h1 className="Header">React Wars</h1>
-      <Characters charactersList={peopleState}/>
-    </div>
+      <Characters charactersList={firstResultState} />
+      <button onClick={nextPage}>Next</button>
+    </CenterDiv>
   );
+
 }
+
 
 export default App;
